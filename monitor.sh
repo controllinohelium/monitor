38a1e6acdc3b6e51fc2488a04e0f4482d7d6a7b3
controllinohelium <86105933+controllinohelium@users.noreply.github.com>
2021-11-30 09:19:34 +0100

    Create monitor.sh

---
+++ monitor.sh
@@ -0,0 +1,23 @@
+#!/bin/bash
+
+# Monitor for Dashboard
+
+if sudo pm2 status | grep -q 'online'; then
+  echo "Dashboard online, all good."
+else
+  echo "Dashboard offline, starting..."
+  cd /home/pi/hotspot_diagnostics
+  sudo pm2 start index.js
+fi
+
+# Montior for Lora Pkt Fwd
+
+if sudo systemctl status lora_pkt_fwd.service | grep -q 'active'; then
+  echo "Packet forwarder online, all good."
+else
+  echo "Packet forwarder offline, starting..."
+  docker stop miner
+  sudo systemctl enable lora_pkt_fwd.service
+  sudo systemctl start lora_pkt_fwd.service
+  docker start miner
+fi
